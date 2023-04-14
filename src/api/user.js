3acfed5397207b18c6521f7326feadecf1d4320e
jinjun.liu <junliujin@163.com>
2023-04-14 19:47:02 +0800

    23.4.14

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,8 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: 'http://localhost:5000/user/login',
+    // url: 'vue-admin-template/user/login',
     method: 'post',
     data
   })
@@ -10,7 +11,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: 'http://localhost:5000/user/getUserInfo',
     method: 'get',
     params: { token }
   })
@@ -18,7 +19,8 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
+    // url: 'http://localhost:5000/user/logout',
+    url: 'vue-admin-template/user/logout',
     method: 'post'
   })
 }

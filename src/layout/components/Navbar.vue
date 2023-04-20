279392d72511f15ed3facb0ffe8fe6ba604ba93c
jinjun.liu <junliujin@163.com>
2023-04-20 15:24:03 +0800

    23.4.20

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -16,12 +16,9 @@
               主页
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
+          <a target="_blank" href="https://www.baidu.com/">
+            <el-dropdown-item>百度</el-dropdown-item>
           </a>
-          <!--<a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">-->
-            <!--<el-dropdown-item>Docs</el-dropdown-item>-->
-          <!--</a>-->
           <el-dropdown-item divided @click.native="loginOut">
             <span style="display:block;">退出</span>
           </el-dropdown-item>
